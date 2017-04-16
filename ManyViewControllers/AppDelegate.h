b00f9ca9542e1a010f593d0bb67afe62d3957ebc
usernamenameuser <writeme@email.ua>
2017-04-16 17:58:53 +0300

    Initial Commit

---
+++ ManyViewControllers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ManyViewControllers
+//
+//  Created by user on 16.04.17.
+//  Copyright © 2017 iPodium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ManyViewControllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ManyViewControllers
+//
+//  Created by user on 16.04.17.
+//  Copyright © 2017 iPodium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
